3e5365018b672518727cbd6a98ce486762f416f1
x9t <imnxtgxxd@gmail.com>
2025-03-13 22:46:38 +0000

    y

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, send_file
+
+app = Flask(__name__)
+
+@app.route('/')
+def landing():
+    return render_template('landing.html')
+
+@app.route('/lobby')
+def lobby():
+    return render_template('looby.html')
+
+@app.route('/lore')
+def lore():
+    return render_template('lore.html')
+
+
+@app.route('/game')
+def game():
+    return render_template('main.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)  
