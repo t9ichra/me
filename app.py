604a5aadbb5e49e66b6bece4b9a3cf2d6d2ed97a
x9t <imnxtgxxd@gmail.com>
2025-03-20 15:15:21 +0000

    eee

--- app.py
+++ app.py
@@ -397,4 +397,4 @@ async def disconnect():
 
 # Main entry point to run the application
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8080)
